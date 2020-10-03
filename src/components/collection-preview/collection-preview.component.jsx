6c1c52815231e700a61b09c0a9ff030e1edf3983
Kripal Shrestha <kripalshr@gmail.com>
2020-10-03 10:27:56 +0545

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -9,13 +9,11 @@ const CollectionPreview = ({ title, items }) => (
     <div className="collection-preview">
         <h1 className="title">{title.toUpperCase()}</h1>
         <div className="preview">
-            {
-                items
-                    .filter((items, idx) => idx < 4)
-                    .map(({ id, ...otherItemProps }) => (
-                        <CollectionItem key={id} {...otherItemProps} />
-                    ))
-            }
+            {items
+                .filter((item, idx) => idx < 4)
+                .map(item => (
+                    <CollectionItem key={item.id} item={item} />
+                ))}
 
         </div>
     </div>
